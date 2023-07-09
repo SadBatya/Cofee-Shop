7985a827947728c442cb8c26cafc6e268b804f55
sadbatya <sadbatya@mail.ru>
2023-07-09 14:33:06 +0200

    fix

--- page2/script.js
+++ page2/script.js
@@ -1,17 +1,21 @@
-const filterBox = document.querySelectorAll('.menu__card')
-const filterButtonsItem = document.querySelector('.buttons')
+const filterBox = document.querySelectorAll('.menu__card');
+const filterButtonsItem = document.querySelector('.buttons');
+const buttons = filterButtonsItem.querySelectorAll('li');
 
+filterButtonsItem.addEventListener('click', (event) => {
+  if (event.target.tagName !== 'LI') return false;
 
+  buttons.forEach((btn) => {
+    btn.style.opacity = 0.6;
+  });
 
+  event.target.style.opacity = 1;
 
-filterButtonsItem.addEventListener('click', event => {
-  if(event.target.tagName !== 'LI') return false
-  
-  let filterClass = event.target.dataset['f']
-  filterBox.forEach( elem => {
-    elem.classList.remove('hide')
-    if(!elem.classList.contains(filterClass) && filterClass !== 'all'){
-      elem.classList.add('hide')
+  let filterClass = event.target.dataset['f'];
+  filterBox.forEach((elem) => {
+    elem.classList.remove('hide');
+    if (!elem.classList.contains(filterClass) && filterClass !== 'all') {
+      elem.classList.add('hide');
     }
-  })
-})
+  });
+});
