26199f242890658cb1c511e21a592296ee6b7b26
sadbatya <sadbatya@mail.ru>
2023-07-09 14:08:46 +0200

    feat: footer

--- page2/script.js
+++ page2/script.js
@@ -1,14 +1,12 @@
 const filterBox = document.querySelectorAll('.menu__card')
 const filterButtonsItem = document.querySelector('.buttons')
-const buttons = filterButtonsItem.querySelectorAll('li')
+
 
 
 
 filterButtonsItem.addEventListener('click', event => {
   if(event.target.tagName !== 'LI') return false
   
-
-
   let filterClass = event.target.dataset['f']
   filterBox.forEach( elem => {
     elem.classList.remove('hide')
